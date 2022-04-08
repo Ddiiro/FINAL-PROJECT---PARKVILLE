2af0002e0fb21d72abce73c89128b1d3401168c1
Ddiiro Joseph <ddiiro92@gmail.com>
2022-04-08 09:22:08 +0300

    adjusted the login page

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -1,18 +1,25 @@
 const express = require('express');
-const passport  = require('passport');
 const router = express.Router();
+const passport = require('passport');
 
 
-router.get('/login', (req, res) => {
-    res.render('login')
+// handling routes of login
+router.get('/login',(req,res)=>{
+    res.render('login') 
 });
 
 router.post('/login', passport.authenticate('local', { failureRedirect: '/login' }),(req, res) => {
 	console.log("This is the login data", req.body)
 	req.session.user = req.user
 	res.redirect('/dashboard');
-})
+});
 
-
+// Logout form
+router.get('/logout', function(req, res) {
+    req.logout();
+    // req.flash('success', 'You are logged out');
+    res.redirect('/login');
+  });
 
 module.exports = router;
+// exporting the router

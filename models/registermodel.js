d90634570f8caabff2b82a92998edad69aebdfcc
Ddiiro Joseph <ddiiro92@gmail.com>
2022-04-08 19:32:11 +0300

    fegister route and model have been designed

--- models/registermodel.js
+++ models/registermodel.js
@@ -1,66 +1,69 @@
-const mongoose = require("mongoose");
-
-const registerSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  receipt: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  time: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  date: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  plateNo: {
-    type: String,
-    required: true,
-    trim: true,
+const mongoose = require('mongoose')
+const Joi = require('joi')
+const clientSchema = mongoose.Schema({
+    name: {
+        type:String,
+        trim:true
+    },
+    receiptNo: {
+        type:String,
+        trim:true
+    },
+    phoneNo: {
+        type:String,
+        trim:true
+    },
+    time: {
+        type:String,
+        trim:true
+    },
+    date: {
+      type: Date,
+      trim:true
+    },
+    plateNo: {
+      type:String,
+      trim:true
   },
   ninNo: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  service: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  shift: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  details: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  duration: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  price: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+    type:String,
+    trim:true
+},
+service: {
+  type:String,
+  trim:true
+},
+shift: {
+  type:String,
+  trim:true
+},
+detail: {
+  type:String,
+  trim:true
+},
+duration: {
+  type:String,
+  trim:true
+},
+price: {
+  type:String,
+  trim:true
+},
 });
 
-const register = module.exports = mongoose.model('register', registerSchema );
+const registerSchema = Joi.object({
+    name:Joi.string().min(5).max(20).required(),
+    receiptNo:Joi.string().min(5).max(20).required(),
+    phoneNo:Joi.string().min(5).max(20).required(),
+    time:Joi.string().required(),
+    date:Joi.string().required(),
+    plateNo:Joi.string().min(3).max(20).required(),
+    ninNo:Joi.string().min(3).max(20).required(),
+    service:Joi.string().min(3).max(20).required(),
+    shift:Joi.string().min(3).max(20).required(),
+    detail:Joi.string().min(8).max(30).required(),
+    duration:Joi.string().min(8).max(20).required(),
+    price:Joi.string().min(8).max(20).required(),
+});
+
+module.exports = mongoose.model('Client', clientSchema);

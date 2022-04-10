039eea33b61141e0125bd9cb49ad2f647d1e3717
Ddiiro Joseph <ddiiro92@gmail.com>
2022-04-10 13:12:12 +0300

    updated the pug pages and css files

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,20 @@
-const express = require('express');
-// const { render } = require('../app');
+// const express = require('express');
+// // const { render } = require('../app');
 
-const router = express.Router();
+// const router = express.Router();
 
-router.get('/login', (req,res) => {
-    res.render('login');
-});
-router.get('/signup', (req,res) => {
-    res.render('signup');
-});
-router.get('/register', (req,res) => {
-    res.render('register');
-});
-router.get('/dashboard', (req,res) => {
-    res.render('dashboard');
-});
+// router.get('/login', (req,res) => {
+//     res.render('login');
+// });
+// router.get('/signup', (req,res) => {
+//     res.render('signup');
+// });
+// router.get('/register', (req,res) => {
+//     res.render('register');
+// });
+// router.get('/dashboard', (req,res) => {
+//     res.render('dashboard');
+// });
 
 // router.post('/', (req,res) =>{
 //     res.render('login', {title:'login'});
@@ -24,4 +24,4 @@ router.get('/dashboard', (req,res) => {
 //     res.render('login', {title:'login'});
 // });
 
-module.exports = router;
+// module.exports = router;
